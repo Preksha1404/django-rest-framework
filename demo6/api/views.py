43ca4d90b646ed8e6dfced256a37ea281a84c4c4
Preksha <ranapreksha8@gmail.com>
2026-01-19 11:14:15 +0530

    Initialize Django project with Student model, API views, and authentication setup

---
+++ demo6/api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[IsAuthenticated]
+
+# class StudentReadOnlyModelViewset(viewsets.ReadOnlyModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
