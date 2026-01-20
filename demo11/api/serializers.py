7d59cf09f8e22af7fab97216cd6250e0a5db98a6
Preksha <ranapreksha8@gmail.com>
2026-01-20 18:00:51 +0530

    Added serializer relations and nested serializers

---
+++ demo11/api/serializers.py
@@ -0,0 +1,17 @@
+from .models import Singer, Song
+from rest_framework import serializers
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'singer', 'duration']
+
+class SingerSerializer(serializers.ModelSerializer):
+    sungby = SongSerializer(many=True, read_only=True)
+    # song = serializers.StringRelatedField(many=True, read_only=True)
+    # song = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # song = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='song-detail')
+    # song = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
+    class Meta:
+        model = Singer
+        fields = ['id', 'name', 'gender', 'sungby']
